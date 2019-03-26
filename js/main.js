b29353985efe34e385567a43f5d5873a4ea96138
bartcis <cis.bart@gmail.com>
2019-03-26 16:19:19 +0000

    Basic Service Worker registered

--- js/main.js
+++ js/main.js
@@ -4,3 +4,22 @@ import '../sass/style.scss';
  * Description
  * @param {type} nameof variable
  */
+class yourClass {
+    constructor() {}
+
+    initialize() {
+        this.registerServiceWorker();
+    }
+
+    registerServiceWorker() {
+        if ('serviceWorker' in navigator) {
+            window.addEventListener('load', function() {
+                navigator.serviceWorker.register('service-worker.js')
+            });
+        }
+    }
+}
+
+const yourPage = new yourClass();
+
+yourPage.initialize();
